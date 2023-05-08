d9bda09f2d8e91fc52c982f00326d3beadd036bd
hyperallday59 <barnescook@mail.weber.edu>
2023-05-08 09:55:47 -0600

    gg

--- Landscapes.py
+++ Landscapes.py
@@ -3,3 +3,9 @@ MOVEMENT = {"north":array([0,1]),
 			"south":array([0,-1]),
 			"east":array([1,0]),
 			"west":array([-1,0])}
+class Area():
+	def __init__(self):
+		self.room_items = []
+		self.usable_items = {}
+		self.allowed_movements = []
+		self.descriptions = {}

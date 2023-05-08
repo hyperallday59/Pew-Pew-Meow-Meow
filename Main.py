7a3a223c26926d92f28f9b54459a081efc93cdc2
hyperallday59 <barnescook@mail.weber.edu>
2023-05-08 10:41:47 -0600

    plesently confused

--- Main.py
+++ Main.py
@@ -2,14 +2,46 @@ import random
 import pickle
 from numpy import array
 from Landscapes import Area, MOVEMENT
-Actions = ("quit", "save", "load", "get", "use", "move")
+ACTIONS = ("quit", "save", "load", "get", "use", "move")
 
+class Hero():
+	def __init__(self):
+		self.__position = (0, 0)
+		self.inventory = []
+		self.name = input("What is your name?\n").title()
+		
+	@property
+	def position(self):
+		return tuple(self.__position)
+		
+	@position.setter
+	def position(self,new):
+		self.__position += new
 
-print("\nAfter months of training you finally return home to the Ninja Cat Village. You've been training to make your family proud and become the ninja you were always meant to be.\nBut... no one is in the village, it's as if it's been deserted.\n")
-print("After searching the village for quite some time you find a letter. It's from your grandfather, the chief of Ninja Cat Village")
-print(f"grandson, the evil ninja cats have invaded our lands stealing our Ninja gear and enslaving us. If you find this we must have all already been taken! Your final test to become a true shinobi will be saving our people and reclaiming our Ninja gear! Luckily I located the Evil Ninja Cat Mansion located deep in the bamboo forest. It's...")
-print('the letter suddenly ends.\n\n"Dang it!" you exclaim\nThey must have invaded the village while your grandfather was writing the letter.')
-print("\nYou waste no time gathering what little supplies you have and embark for the bamboo forest. After a day or two of treacherous hiking you've finally reached it!\nThe entrance to the bamboo forest!")
+def valid_input(prompt = "What would you like to do? "):
+	print("\t--Options--")
+	response = None
+	while response not in ACTIONS:
+		print(f"Actions:\n{ACTIONS}")
+		response = input(prompt).lower()
+	return response
 
-def main(hero):
-    print("yay")
+def save():
+	'''save the player object and rooms dictionary to a file'''
+	with open('game.dat','wb') as f:
+		pickle.dump(Hero,f)
+		pickle.dump(Landscape,f)
+	print("Game saved!")
+
+def load():
+	'''load the hero object and rooms dictionary from a file'''
+	#using global variables to reduce size
+	global Hero
+	global Landscape
+	try:
+		with open("game.dat",'rb') as f:
+			Hero = pickle.load(f)
+			Landscape = pickle.load(f)
+		print("Game loaded!")
+	except FileNotFoundError:
+		print("Game file not found!")

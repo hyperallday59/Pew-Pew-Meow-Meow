c4ccb5b2d9a670f494f17c4e4cd382512ca7b96d
hyperallday59 <barnescook@mail.weber.edu>
2023-05-08 10:05:20 -0600

    working through

--- Landscapes.py
+++ Landscapes.py
@@ -5,7 +5,19 @@ MOVEMENT = {"north":array([0,1]),
 			"west":array([-1,0])}
 class Area():
 	def __init__(self):
-		self.room_items = []
+		self.area_items = []
 		self.usable_items = {}
 		self.allowed_movements = []
 		self.descriptions = {}
+	
+	def get_item(self, hero):
+		if self.area_items:
+			item = input("What item would you like to 'get'? ").lower()
+			if item in self.area_items:
+				print(f"You pickup the {item}.")
+				hero.inventory.append(item)
+				self.area_items.remove(item)
+			else:
+				print(f"Couldn't find {item} in room.")
+		else:
+			print("There's nothing to pickup here!")
